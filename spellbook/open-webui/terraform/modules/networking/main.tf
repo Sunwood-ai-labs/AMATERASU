1ffc4fa9ef4dbc2a2028c7c4661ddda2fc0dedd2
Maki <sunwood.ai.labs@gmail.com>
2024-11-11 02:09:46 +0900

    ♻️ refactor: HTTP リスナーを HTTPS リダイレクトに変更
    - HTTP リスナーの設定を、HTTP から HTTPS へのリダイレクトに変更しました。
    - セキュリティ強化のため、HTTP へのリクエストを自動的に HTTPS にリダイレクトします。
    - `aws_lb_listener.http` リソースの `default_action` を `fixed-response` から `redirect` に変更しました。
    - リダイレクトはステータスコード 301 (永続的リダイレクト) を使用します。

--- spellbook/open-webui/terraform/modules/networking/main.tf
+++ spellbook/open-webui/terraform/modules/networking/main.tf
@@ -177,15 +177,14 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Please use HTTPS"
-      status_code  = "200"
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
-
 # HTTPSリスナー
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
