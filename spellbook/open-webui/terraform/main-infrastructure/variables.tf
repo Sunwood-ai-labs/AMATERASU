93bff996d8862ac28840ca8873d1924f40f0a1e2
Maki <sunwood.ai.labs@gmail.com>
2024-11-19 21:56:01 +0900

    ⚙️ chore: 変数定義
    - terraformに必要な変数を定義しました。
    - これらの値を変更することで、異なる環境への展開を容易に行うことができます。

---
+++ spellbook/open-webui/terraform/main-infrastructure/variables.tf
@@ -0,0 +1,69 @@
+# variables.tf
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "project_name" {
+  description = "Name of the project, used as a prefix for all resources"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the existing VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "ID of the first public subnet"
+  type        = string
+}
+
+variable "public_subnet_2_id" {
+  description = "ID of the second public subnet"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "ID of the existing security group"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "domain" {
+  description = "Base domain name"
+  type        = string
+  default     = "sunwood-ai-labs.click"
+}
+
+variable "subdomain" {
+  description = "Subdomain prefix"
+  type        = string
+  default     = "amaterasu-open-web-ui-dev"
+}
