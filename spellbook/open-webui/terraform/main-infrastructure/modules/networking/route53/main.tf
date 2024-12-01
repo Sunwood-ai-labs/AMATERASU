90d60a85dbe7d77e2c3ddd065844e2745406f7d8
Maki <sunwood.ai.labs@gmail.com>
2024-12-01 20:22:19 +0900

    ♻️ refactor: Route53 Private Hosted Zone の管理方法変更
    - Route53 Private Hosted Zone をリソースとして作成するのではなく、データソースを使用して既存のゾーンを参照するように変更しました。これにより、既存のゾーンを再利用し、不要な変更を避けます。

--- spellbook/open-webui/terraform/main-infrastructure/modules/networking/route53/main.tf
+++ spellbook/open-webui/terraform/main-infrastructure/modules/networking/route53/main.tf
@@ -1,22 +1,15 @@
 # modules/networking/route53/main.tf
 
-# プライベートホストゾーンの作成
-resource "aws_route53_zone" "private" {
-  name = var.domain
-
-  vpc {
-    vpc_id = var.vpc_id
-  }
-
-  tags = {
-    Name = "${var.project_name}-private-zone"
-  }
+# プライベートホストゾーンのデータソース
+data "aws_route53_zone" "private" {
+  name         = var.domain
+  private_zone = true
+  vpc_id       = var.vpc_id  # vpcブロックの代わりにvpc_idを使用
 }
 
 # プライベートホストゾーンにALBのエントリを追加
-# これにより、VPC内からのDNSクエリは内部IPにルーティングされます
 resource "aws_route53_record" "private" {
-  zone_id = aws_route53_zone.private.id
+  zone_id = data.aws_route53_zone.private.zone_id
   name    = "${var.subdomain}.${var.domain}"
   type    = "A"
 
@@ -25,11 +18,13 @@ resource "aws_route53_record" "private" {
     zone_id                = var.alb_zone_id
     evaluate_target_health = true
   }
+
+  depends_on = [var.alb_depends_on]
 }
 
-# パブリックホストゾーンのレコード（既存）
+# パブリックホストゾーンのレコード
 resource "aws_route53_record" "public" {
-  zone_id = var.route53_zone_id  # パブリックゾーンID
+  zone_id = var.route53_zone_id
   name    = "${var.subdomain}.${var.domain}"
   type    = "A"
 
@@ -38,4 +33,6 @@ resource "aws_route53_record" "public" {
     zone_id                = var.alb_zone_id
     evaluate_target_health = true
   }
+
+  depends_on = [var.alb_depends_on]
 }
