2ffc3ee4d38fae98b9cb9a317bb51fe6d721cdfc
Maki <sunwood.ai.labs@gmail.com>
2024-12-01 16:41:36 +0900

    ✨ feat: ALB出力の簡素化
    - パブリックALBとインターナルALBの出力項目を統合しました。
    - 単一のALBからの出力に簡素化し、冗長性を排除しました。

--- spellbook/open-webui/terraform/main-infrastructure/modules/networking/alb/outputs.tf
+++ spellbook/open-webui/terraform/main-infrastructure/modules/networking/alb/outputs.tf
@@ -1,37 +1,16 @@
 # modules/networking/alb/outputs.tf
 
 output "alb_target_group_arn" {
-  description = "ARN of the public ALB target group"
-  value       = aws_lb_target_group.public.arn
+  description = "ARN of the ALB target group"
+  value       = aws_lb_target_group.main.arn
 }
 
 output "alb_dns_name" {
-  description = "DNS name of the public ALB"
-  value       = aws_lb.public.dns_name
+  description = "DNS name of the ALB"
+  value       = aws_lb.main.dns_name
 }
 
 output "alb_zone_id" {
-  description = "Zone ID of the public ALB"
-  value       = aws_lb.public.zone_id
-}
-
-output "alb_target_group_name" {
-  description = "Name of the public ALB target group"
-  value       = aws_lb_target_group.public.name
-}
-
-# インターナルALBの出力も追加
-output "internal_alb_target_group_arn" {
-  description = "ARN of the internal ALB target group"
-  value       = aws_lb_target_group.internal.arn
-}
-
-output "internal_alb_dns_name" {
-  description = "DNS name of the internal ALB"
-  value       = aws_lb.internal.dns_name
-}
-
-output "internal_alb_zone_id" {
-  description = "Zone ID of the internal ALB"
-  value       = aws_lb.internal.zone_id
+  description = "Zone ID of the ALB"
+  value       = aws_lb.main.zone_id
 }
