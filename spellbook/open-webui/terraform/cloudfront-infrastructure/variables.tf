6f522cf18beaa920fd1fa214ba9f768e937e3cf5
Maki <sunwood.ai.labs@gmail.com>
2024-12-04 23:39:36 +0900

    🧱 chore: CloudFront環境変数定義
    - CloudFrontインフラストラクチャに必要な環境変数を定義しました。
    - プロジェクト名、AWSリージョン、オリジンドメイン、VPC ID、パブリックサブネットID、セキュリティグループID、許可IP範囲を定義しています。
    - デフォルト値を設定し、必要に応じて変更できます。

---
+++ spellbook/open-webui/terraform/cloudfront-infrastructure/variables.tf
@@ -0,0 +1,36 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region for the resources"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "origin_domain" {
+  description = "Domain name of the origin (EC2 instance)"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the existing VPC"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "ID of the public subnet"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "ID of the existing security group"
+  type        = string
+}
+
+variable "allowed_ip_ranges" {
+  description = "List of IP ranges to allow access to CloudFront (in CIDR notation)"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]  # デフォルトですべてのIPを許可（開発用）
+}
