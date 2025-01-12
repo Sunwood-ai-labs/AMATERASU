ae0afdb6657cbcecf592be33f2d858d720867642
Maki <sunwood.ai.labs@gmail.com>
2025-01-12 21:05:05 +0900

    ♻️ refactor: networkingモジュールの修正
    - ALBとCloudFront関連のモジュール呼び出しを削除し、モジュール構成を簡素化しました。

--- spellbook/open-webui/terraform/main-infrastructure/modules/networking/main.tf
+++ spellbook/open-webui/terraform/main-infrastructure/modules/networking/main.tf
@@ -2,7 +2,7 @@
 
 module "core" {
   source = "./core"
-
+  
   project_name      = var.project_name
   aws_region        = var.aws_region
   vpc_id            = var.vpc_id
@@ -13,29 +13,4 @@ module "core" {
   domain           = var.domain
   subdomain        = var.subdomain
   enable_health_check = false
-}
-
-# CloudFrontモジュール
-module "cloudfront" {
-  source = "./cloudfront"
-
-  project_name     = var.project_name
-  domain          = var.domain
-  subdomain       = var.subdomain
-  alb_dns_name    = module.core.alb_dns_name
-  alb_zone_id     = module.core.alb_zone_id
-  route53_zone_id = module.core.route53_zone_id
-  
-  # 既存のRoute53レコードの情報を渡す
-  existing_record_id     = module.core.route53_internal_record_id
-  wait_for_existing_record = true
-
-  providers = {
-    aws.us_east_1 = aws.us_east_1
-  }
-
-  depends_on = [
-    module.core,
-    module.core.route53_internal_record_id
-  ]
 }
