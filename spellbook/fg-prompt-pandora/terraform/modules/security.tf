5dfff92bdc3df210c78a40b54b0b4791ddb9a0c0
Maki <sunwood.ai.labs@gmail.com>
2025-01-22 23:57:10 +0900

    🛡️ chore: セキュリティグループのポート番号を80に変更
    - セキュリティグループのインバウンドルールで、ポート番号を8501から80に変更しました。これにより、ポート80からのアクセスのみ許可するように設定しました。

--- spellbook/fg-prompt-pandora/terraform/modules/security.tf
+++ spellbook/fg-prompt-pandora/terraform/modules/security.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "ecs_tasks" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 8501
-    to_port         = 8501
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
     security_groups = var.security_group_ids
   }
